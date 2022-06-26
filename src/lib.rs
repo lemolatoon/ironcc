a8d8242af3d3db9d43e1c2f5fd039d6132b2a067
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-06-26 19:08:21 +0900

    Add `Position` struct and use it while tokenize

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,17 @@ pub mod tokenize;
 #[cfg(test)]
 mod tests {
 
+    use crate::tokenize::{kind_eq, tokenize_and_kinds, Position};
+
     use super::*;
 
     #[test]
-    fn tokenize_test() {
+    fn tokenize_plus_minus_test() {
         use crate::tokenize::{tokenize, BinOpToken, Token, TokenKind};
         let input = String::from("1 + 4 -       909");
-        assert_eq!(
-            tokenize(input),
-            tokens!(
+        assert!(kind_eq(
+            &tokenize(input),
+            &tokens!(
                 TokenKind::Num(1),
                 TokenKind::BinOp(BinOpToken::Plus),
                 TokenKind::Num(4),
@@ -19,12 +21,12 @@ mod tests {
                 TokenKind::Num(909),
                 TokenKind::Eof
             )
-        );
+        ));
 
         let input = String::from("0\t + 5+1+9-3 -  \n     909");
-        assert_eq!(
-            tokenize(input),
-            tokens!(
+        assert!(kind_eq(
+            &tokenize(input),
+            &tokens!(
                 TokenKind::Num(0),
                 TokenKind::BinOp(BinOpToken::Plus),
                 TokenKind::Num(5),
@@ -37,6 +39,70 @@ mod tests {
                 TokenKind::BinOp(BinOpToken::Minus),
                 TokenKind::Num(909),
                 TokenKind::Eof
+            )
+        ));
+
+        let input = String::from("0 + 0 + 11  -4");
+        assert_eq!(
+            tokenize(input)
+                .into_iter()
+                .map(|token| token.kind())
+                .collect::<Vec<_>>(),
+            tokens!(
+                TokenKind::Num(0),
+                TokenKind::BinOp(BinOpToken::Plus),
+                TokenKind::Num(0),
+                TokenKind::BinOp(BinOpToken::Plus),
+                TokenKind::Num(11),
+                TokenKind::BinOp(BinOpToken::Minus),
+                TokenKind::Num(4),
+                TokenKind::Eof
+            )
+            .into_iter()
+            .map(|token| token.kind())
+            .collect::<Vec<_>>()
+        );
+
+        let input = String::from("10101010101 + 0 + 11  -4");
+        assert_eq!(
+            tokenize_and_kinds(input),
+            token_kinds!(
+                TokenKind::Num(10101010101),
+                TokenKind::BinOp(BinOpToken::Plus),
+                TokenKind::Num(0),
+                TokenKind::BinOp(BinOpToken::Plus),
+                TokenKind::Num(11),
+                TokenKind::BinOp(BinOpToken::Minus),
+                TokenKind::Num(4),
+                TokenKind::Eof
+            )
+        )
+    }
+
+    #[test]
+    fn tokenize_pos_test() {
+        use crate::tokenize::{tokenize, BinOpToken, Position, Token, TokenKind};
+        let input = String::from("1 +1");
+        assert_eq!(
+            tokenize(input),
+            token_poses!(
+                (TokenKind::Num(1), Position::new(0, 0)),
+                (TokenKind::BinOp(BinOpToken::Plus), Position::new(2, 0)),
+                (TokenKind::Num(1), Position::new(3, 0)),
+                (TokenKind::Eof, Position::new(4, 0))
+            )
+        );
+
+        let input = String::from("1 +1\n\t+5");
+        assert_eq!(
+            tokenize(input),
+            token_poses!(
+                (TokenKind::Num(1), Position::new(0, 0)),
+                (TokenKind::BinOp(BinOpToken::Plus), Position::new(2, 0)),
+                (TokenKind::Num(1), Position::new(3, 0)),
+                (TokenKind::BinOp(BinOpToken::Plus), Position::new(1, 1)),
+                (TokenKind::Num(5), Position::new(2, 1)),
+                (TokenKind::Eof, Position::new(3, 1))
             )
         );
     }
--- src/tokenize.rs
+++ src/tokenize.rs
@@ -3,12 +3,24 @@ use std::iter::Peekable;
 pub fn tokenize(input: String) -> Vec<Token> {
     let mut tokens = Vec::new();
     let mut input_chars = input.chars().peekable();
+    let mut pos = Position::default(); // 0, 0
     while let Some(c) = input_chars.next() {
         match c {
             // skip white spaces
-            ' ' | '\n' | '\t' => continue,
-            '+' => tokens.push(Token::new(TokenKind::BinOp(BinOpToken::Plus))),
-            '-' => tokens.push(Token::new(TokenKind::BinOp(BinOpToken::Minus))),
+            ' ' | '\t' => {
+                pos.next_char();
+            }
+            '\n' => {
+                pos.next_line();
+            }
+            '+' => tokens.push(Token::new(
+                TokenKind::BinOp(BinOpToken::Plus),
+                pos.next_char(),
+            )),
+            '-' => tokens.push(Token::new(
+                TokenKind::BinOp(BinOpToken::Minus),
+                pos.next_char(),
+            )),
             '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
                 let mut number = String::from(c);
                 while let Some(&('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')) =
@@ -16,15 +28,19 @@ pub fn tokenize(input: String) -> Vec<Token> {
                 {
                     number.push(input_chars.next().unwrap())
                 }
+                let len_token = number.len();
                 let num = number
                     .parse::<usize>()
                     .expect("Currently support only a number literal.");
-                tokens.push(Token::new(TokenKind::Num(num as isize)))
+                tokens.push(Token::new(
+                    TokenKind::Num(num as isize),
+                    pos.next_token(len_token),
+                ))
             }
             _ => panic!("Unexpected char while tokenize: {}", c),
         }
     }
-    tokens.push(Token::new(TokenKind::Eof));
+    tokens.push(Token::new(TokenKind::Eof, pos.next_token(0)));
 
     tokens
 }
@@ -45,13 +61,55 @@ pub enum BinOpToken {
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Token {
     pub kind: Box<TokenKind>,
+    pub pos: Position,
 }
 
 impl Token {
-    pub fn new(token_kind: TokenKind) -> Self {
+    pub fn new(token_kind: TokenKind, pos: Position) -> Self {
         Self {
             kind: Box::new(token_kind),
+            pos,
         }
+    }
+
+    pub fn kind_eq(&self, rhs: &Token) -> bool {
+        self.kind == rhs.kind
+    }
+
+    pub fn kind(self) -> Box<TokenKind> {
+        self.kind
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Default)]
+pub struct Position {
+    n_char: usize,
+    n_line: usize,
+}
+
+impl Position {
+    pub fn new(n_char: usize, n_line: usize) -> Self {
+        Self { n_char, n_line }
+    }
+
+    pub fn next_line(&mut self) -> Self {
+        let return_struct = self.clone();
+        self.n_char = 0;
+        self.n_line += 1;
+        return_struct
+    }
+
+    // increment self.n_char and return not incremented, cloned Position struct
+    pub fn next_char(&mut self) -> Self {
+        let return_struct = self.clone();
+        self.n_char += 1;
+        return_struct
+    }
+
+    pub fn next_token(&mut self, len_token: usize) -> Self {
+        let return_struct = self.clone();
+        self.n_char += len_token;
+        return_struct
     }
 }
 
@@ -94,20 +152,58 @@ impl<I: Iterator<Item = Token>> Iterator for TokenStream<I> {
     }
 }
 
-trait Expect {
-    type Item;
-
-    fn expect(kind: Self::Item) -> bool;
-}
-
 #[cfg(test)]
 #[macro_export]
 macro_rules! tokens {
     ( $( $token_kind:expr ), *) => {{
         let mut temp_vec = Vec::new();
         $(
-            temp_vec.push(Token::new($token_kind));
+            let pos = crate::tokenize::Position::default();
+            temp_vec.push(Token::new($token_kind, pos));
         )*
         temp_vec
     }};
+}
+#[cfg(test)]
+#[macro_export]
+macro_rules! token_poses {
+    ( $( ($token_kind:expr, $pos:expr) ), *) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push(Token::new($token_kind, $pos));
+        )*
+        temp_vec
+    }};
+}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! token_kinds {
+    ( $( $token_kind:expr ), *) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            let pos = crate::tokenize::Position::default();
+            temp_vec.push(Token::new($token_kind, pos));
+        )*
+        temp_vec
+            .into_iter()
+            .map(|token| token.kind())
+            .collect::<Vec<_>>()
+    }};
+}
+
+#[cfg(test)]
+pub fn kind_eq(lhs: &Vec<Token>, rhs: &Vec<Token>) -> bool {
+    lhs.into_iter()
+        .zip(rhs.into_iter())
+        .fold(true, |acc, (l_token, r_token)| {
+            acc && l_token.kind_eq(r_token)
+        })
+}
+
+pub fn tokenize_and_kinds(input: String) -> Vec<Box<TokenKind>> {
+    tokenize(input)
+        .into_iter()
+        .map(|token| token.kind())
+        .collect()
 }
