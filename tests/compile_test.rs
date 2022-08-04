5257857b108ca1be3aa23099d81249e3c0052970
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-08-05 03:56:58 +0900

    Add tests

--- tests/compile_test.rs
+++ tests/compile_test.rs
@@ -263,3 +263,62 @@ fn scopes() {
         tester.conv_program()
     );
 }
+
+#[test]
+fn global_variable() {
+    let src = "
+    int global1;
+    int main() {
+        int global1;
+    }";
+    let mut tester = CachedProcesser::new(src);
+    let result = tester.conv_program();
+    assert!(
+        matches!(
+            result,
+            Err(CompileError {
+                kind: CompileErrorKind::AnalyzeError(AnalyzeErrorKind::RedefinedError(
+                    _,
+                    _,
+                    VariableKind::Global,
+                )),
+                src: _,
+            })
+        ),
+        "{:?}",
+        result,
+    );
+
+    let src = "
+    int global1;
+    int (*global1)[3];
+    int main() {
+    }";
+    let mut tester = CachedProcesser::new(src);
+    let result = tester.conv_program();
+    assert!(
+        matches!(
+            result,
+            Err(CompileError {
+                kind: CompileErrorKind::AnalyzeError(AnalyzeErrorKind::RedefinedError(
+                    _,
+                    _,
+                    VariableKind::Global,
+                )),
+                src: _,
+            })
+        ),
+        "{:?}",
+        result,
+    );
+
+    let src = "
+    int main() {
+        int (*global1)[3];
+    }
+    int global1;
+    ";
+    let mut tester = CachedProcesser::new(src);
+    let result = tester.conv_program();
+    assert!(matches!(result, Ok(_)), "{:?}", result,);
+}
