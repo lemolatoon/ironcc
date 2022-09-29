1c5974fa0f7d88dcea5beb0f1a9481a80ab7faee
lemolatoon <lemolatoon@users.noreply.github.com>
2022-09-29 04:42:38 +0000

    deploy: 632a47138437d4d7fc37827bbe98b99bba6b03de

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["ironcc"] = {"name":"","files":["analyze.rs","common.rs","error.rs","generate.rs","lib.rs","parse.rs","preprocess.rs","tokenize.rs"]};
+var sourcesIndex = JSON.parse('{\
+"ironcc":["",[],["analyze.rs","common.rs","error.rs","generate.rs","lib.rs","parse.rs","preprocess.rs","tokenize.rs"]]\
+}');
 createSourceSidebar();
