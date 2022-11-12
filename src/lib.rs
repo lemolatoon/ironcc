f3a9f3a2676b6ee6140999254f34055ca935491c
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-11-12 16:57:53 +0900

    Add more export functions

--- src/lib.rs
+++ src/lib.rs
@@ -17,21 +17,22 @@ pub mod parse;
 pub mod preprocess;
 pub mod tokenize;
 
+use crate::generate::Generator;
+use analyze::Analyzer;
 use error::CompileError;
+use parse::Parser;
+use preprocess::Preprocessor;
+use preprocess::PreprocessorTokenContainerStream;
+use preprocess::PreprocessorTokenStream;
+use preprocess::SrcCursor;
+use std::io::BufWriter;
+use std::rc::Rc;
+use tokenize::FileInfo;
+use tokenize::TokenStream;
+use tokenize::Tokenizer;
 
 pub fn preprocess_and_compile(mut input: String) -> Result<String, CompileError> {
-    use crate::generate::Generator;
-    use analyze::Analyzer;
-    use parse::Parser;
-    use preprocess::Preprocessor;
-    use preprocess::PreprocessorTokenContainerStream;
-    use preprocess::PreprocessorTokenStream;
-    use preprocess::SrcCursor;
-    use std::io::BufWriter;
-    use std::rc::Rc;
-    use tokenize::FileInfo;
-    use tokenize::TokenStream;
-    use tokenize::Tokenizer;
+    use std::io::Write;
     input.push('\n');
 
     let file_name = "src.c".to_string();
@@ -39,9 +40,10 @@ pub fn preprocess_and_compile(mut input: String) -> Result<String, CompileError>
     let mut preprocessor = Preprocessor::new(main_file_info.clone(), "include");
     let tokens = preprocessor.preprocess(&mut SrcCursor::new(main_file_info), None)?;
     let stream = PreprocessorTokenStream::new(tokens.into_iter());
+    let container_stream = PreprocessorTokenContainerStream::new(stream.collect());
 
-    let mut tokenizer = Tokenizer::new(PreprocessorTokenContainerStream::new(stream.collect()));
-    let file_info = Rc::new(FileInfo::new(file_name.clone(), input)); // TODO: remove this clone, by all input info around substituted with Rc
+    let mut tokenizer = Tokenizer::new(container_stream);
+    let file_info = Rc::new(FileInfo::new(file_name, input));
     let tokens = tokenizer.tokenize(&file_info)?;
     let mut token_stream = TokenStream::new(tokens.into_iter());
 
@@ -51,7 +53,6 @@ pub fn preprocess_and_compile(mut input: String) -> Result<String, CompileError>
     let mut analyzer = Analyzer::new();
     let converted_program = analyzer.traverse_program(program)?;
 
-    use std::io::Write;
     let mut buf_writer = BufWriter::new(Vec::new());
     let mut generater = Generator::new();
     generater.gen_head(&mut buf_writer, converted_program)?;
@@ -59,4 +60,49 @@ pub fn preprocess_and_compile(mut input: String) -> Result<String, CompileError>
 
     let asm = String::from_utf8(buf_writer.into_inner().unwrap()).unwrap();
     Ok(asm)
+}
+
+pub fn preprocessed_source(mut input: String) -> Result<String, CompileError> {
+    input.push('\n');
+
+    let file_name = "src.c".to_string();
+    let main_file_info = Rc::new(FileInfo::new(file_name, input.clone()));
+    let mut preprocessor = Preprocessor::new(main_file_info.clone(), "include");
+    let tokens = preprocessor.preprocess(&mut SrcCursor::new(main_file_info), None)?;
+    let stream = PreprocessorTokenStream::new(tokens.into_iter());
+    let container_stream = PreprocessorTokenContainerStream::new(stream.collect());
+
+    Ok(container_stream.map(|(_, ch)| ch).collect())
+}
+
+pub fn tokens(
+    mut input: String,
+) -> Result<Vec<tokenize::Token<tokenize::TokenKind>>, CompileError> {
+    input.push('\n');
+
+    let file_name = "src.c".to_string();
+    let main_file_info = Rc::new(FileInfo::new(file_name.clone(), input.clone()));
+    let mut preprocessor = Preprocessor::new(main_file_info.clone(), "include");
+    let tokens = preprocessor.preprocess(&mut SrcCursor::new(main_file_info), None)?;
+    let stream = PreprocessorTokenStream::new(tokens.into_iter());
+    let container_stream = PreprocessorTokenContainerStream::new(stream.collect());
+
+    let mut tokenizer = Tokenizer::new(container_stream);
+    let file_info = Rc::new(FileInfo::new(file_name, input));
+    tokenizer.tokenize(&file_info)
+}
+
+pub fn parsed_ast(input: String) -> Result<parse::Program, CompileError> {
+    let tokens = tokens(input)?;
+    let mut token_stream = TokenStream::new(tokens.into_iter());
+
+    let mut parser = Parser::new();
+    let program = parser.parse_program(&mut token_stream)?;
+    Ok(program)
+}
+
+pub fn converted_ast(input: String) -> Result<analyze::ConvProgram, CompileError> {
+    let ast = parsed_ast(input)?;
+    let mut analyzer = Analyzer::new();
+    analyzer.traverse_program(ast)
 }
