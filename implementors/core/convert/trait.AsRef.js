00c72c5009375d705c2ee1c59b1a9b7198efa5c3
lemolatoon <lemolatoon@users.noreply.github.com>
2022-12-27 07:22:07 +0000

    deploy: ef725fcb8de0186963d93f8f3bf0116e302a5a3e

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["ironcc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.64.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"ironcc/generate/enum.RegSize.html\" title=\"enum ironcc::generate::RegSize\">RegSize</a>","synthetic":false,"types":["ironcc::generate::RegSize"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"ironcc":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"ironcc/generate/enum.RegSize.html\" title=\"enum ironcc::generate::RegSize\">RegSize</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
